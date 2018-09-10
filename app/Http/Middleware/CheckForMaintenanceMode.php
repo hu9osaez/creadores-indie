59910b03933c585a7d02b35b0de4e457d0549389
Hugo Sáez <hu9o.saez@gmail.com>
2018-09-10 11:01:26 -0300

    Initial commit

---
+++ app/Http/Middleware/CheckForMaintenanceMode.php
@@ -0,0 +1,13 @@
+<?php namespace CreadoresIndie\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode as Middleware;
+
+class CheckForMaintenanceMode extends Middleware
+{
+    /**
+     * The URIs that should be reachable while maintenance mode is enabled.
+     *
+     * @var array
+     */
+    protected $except = [];
+}
---
+++ app/Http/Middleware/EncryptCookies.php
@@ -0,0 +1,13 @@
+<?php namespace CreadoresIndie\Http\Middleware;
+
+use Illuminate\Cookie\Middleware\EncryptCookies as Middleware;
+
+class EncryptCookies extends Middleware
+{
+    /**
+     * The names of the cookies that should not be encrypted.
+     *
+     * @var array
+     */
+    protected $except = [];
+}
