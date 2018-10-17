890595bc2df8afc226e0e82f099af908efe0e1a6
Hugo Sáez <hu9o.saez@gmail.com>
2018-10-16 22:30:22 -0300

    Order profile's discussions by created date

--- app/Http/Controllers/Front/ProfileController.php
+++ app/Http/Controllers/Front/ProfileController.php
@@ -8,7 +8,10 @@ class ProfileController extends Controller
     public function show($username)
     {
         $user = User::with('discussions')->whereUsername($username)->firstOrFail();
-        $discussions = $user->discussions()->with('category', 'user')->paginate();
+        $discussions = $user->discussions()
+            ->with('category', 'user')
+            ->latest()
+            ->paginate();
 
         return view('front.profile.show', compact('user', 'discussions'));
     }
