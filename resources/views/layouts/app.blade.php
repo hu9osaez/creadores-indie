0222ac21fcca5918bd7be73b96c7688984f42376
Hugo Sáez <hu9o.saez@gmail.com>
2018-10-23 22:51:14 -0300

    Fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
     @yield('seo')
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') . '?v=' . Version::build() }}" rel="stylesheet">
     @stack('custom-css')
 
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
